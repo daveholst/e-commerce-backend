8665c3a206abde802677aa277b450d4aebece363
Dave Holst <daveholst@gmail.com>
2021-05-29 12:11:43 +0800

    SPEC: productTag + tag models working + relationships + seeding

--- models/index.js
+++ models/index.js
@@ -14,8 +14,13 @@ Category.hasMany(Product, {
   foreignKey: 'category_id',
 })
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: 'product_tag'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: 'product_tag'
+})
 
 module.exports = {
   Product,
