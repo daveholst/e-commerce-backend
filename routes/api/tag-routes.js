7c73936d251faecfc025280660e84bf3d30dbcc1
Dave Holst <daveholst@gmail.com>
2021-05-29 20:16:37 +0800

    SPEC: get all tags, single tag and create all working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,18 +3,42 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// find all tags
+router.get('/', async (req, res) => {
+  try {
+    res.json(await Tag.findAll({ include: [Product] }));
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+// find a single tag by its `id`
+router.get('/:id', async (req, res) => {
+  try {
+    const queryResults = await Tag.findByPk(req.params.id, { include: [Product] });
+    queryResults ? res.json(queryResults) : res.status(404).json({ message: "Could not find a Tag with that ID" });
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+// create a new tag take object in { addtribute: data }
+router.post('/', async (req, res) => {
+  try {
+    if (req.body.tag_name) {
+      await Tag.create(req.body);
+      console.log(req.body);
+      res.status(200).json({ message: `Added Tag ${req.body.tag_name}` })
+    } else {
+      res.status(400).json(
+        { message: 'POST request for new tag requires an attribute object {tag_name: \'tag Name\'}' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+  }
 });
 
 router.put('/:id', (req, res) => {
