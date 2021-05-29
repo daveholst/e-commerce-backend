fb7c06afa19ec16f712d8acaba51fbe8b1de4752
Dave Holst <daveholst@gmail.com>
2021-05-29 18:18:42 +0800

    SPEC: all categoryRoutes working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -28,11 +28,12 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     if (req.body.category_name) {
-      Category.create(req.body);
+      await Category.create(req.body);
       console.log(req.body);
       res.status(200).json({ message: `Added Catgeory ${req.body.category_name}` })
     } else {
-      res.status(400).json({message: 'POST request for new category requires an attribute object {category_name: \'catName\'}' })
+      res.status(400).json(
+        { message: 'POST request for new category requires an attribute object {category_name: \'catName\'}' });
     }
   } catch (error) {
     console.error(error);
@@ -40,12 +41,39 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    if (req.body.category_name) {
+      await Category.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      })
+      res.status(200).json({message: `Category ID: ${req.params.id} updated. `})
+    } else {
+      res.status(400).json(
+        { message: 'PUT request for new category requires an attribute object {category_name: \'catName\'}' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const queryResponse = await Category.destroy({ where: { id: req.params.id } });
+    if (queryResponse) {
+      res.status(200).json({ message: `Deleted Category ${req.params.id}` });
+    } else {
+      res.status(400).json({ message: `Category ID:${req.params.id} could not be found.` });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+  }
 });
 
 module.exports = router;
